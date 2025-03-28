222ceeaae2a3cdedbc9f56d978e4a4a416463f4a
July <danielitha345ra@gmail.com>
2025-03-28 13:16:27 -0500

    diagrmas mer recuperados

--- Diagram/Class1.cs
+++ Diagram/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Diagram
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram2
 {
     public class Class1
     {
-
     }
 }
